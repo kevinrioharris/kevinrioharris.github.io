da63b03246b031547aaacce1aec02766568a6995
Kevin Rio H <kevinrioharris@gmail.com>
2026-07-11 00:42:35 +0700

    Feat: Modification background

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ResumePage from './pages/ResumePage';
+import ScrollToTop from './components/ScrollToTop';
 import './styles/App.css';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
